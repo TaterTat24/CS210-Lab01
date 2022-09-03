31ed00629c4a6c64e5a0d3b067ec40eff614811d
Benjamin <C24Benjamin.Tat@afacademy.af.edu>
2022-09-03 15:16:31 -0600

    Lab 1 Final

--- hello.c
+++ hello.c
@@ -1,23 +1,23 @@
 /** hello.c
  * =============================================================
- * Name: Your Name, Date
- * Section:  Your Section
+ * Name: Benjamin Tat
+ * Section:  T6
  * Project:  Lab 01
  * Purpose:  Write a simple program in the C Language
  * ============================================================= */
 
 // Part 1:  What do you have to import in order to use printf?
-
+#include <stdio.h>
 
 // Part 2:  What is the name of the function that C always runs when it starts?
+int main(void){
 
+// Part 3:  How do you print a phrase?
+printf("Hello, world! My name is Cadet Tat.\n");
 
-  // Part 3:  How do you print a phrase?
-  
-  
-  // Part 4:  What does the main function always return?
-  
-
+// Part 4:  What does the main function always return?
+}
+// return 0
 
 // Part 5:  By the time you get here, you are done.  
 // Open a terminal (in Visual Studio Code, Go to Terminal --> New Terminal)
